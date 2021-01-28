23d4d3ae3046f05062ca6b85d02afd8eda4a85b4
joshua-orderdaily <joshua@orderdaily.nl>
2021-01-28 22:50:12 +0100

    Added api request function

--- pokebowl/lib/main.dart
+++ pokebowl/lib/main.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:pokebowl/cartScreen.dart';
 import 'package:pokebowl/infoScreen.dart';
 import 'package:pokebowl/baseChoice.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:pokebowl/halfCircle.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MyApp());
@@ -13,9 +16,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-    home: new HomeScreen()
-    );
+    return MaterialApp(home: new HomeScreen());
   }
 }
 
@@ -73,12 +74,19 @@ class HomeScreen extends StatelessWidget {
               Text('PokeBowlGo', style: TextStyle(fontSize: 30)),
               RichText(
                 text: TextSpan(
-                  text: 'Create ', style: TextStyle(fontSize: 30, color: Colors.black),
-                  children: <TextSpan>[
-                    TextSpan(text: 'your ', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.black)),
-                    TextSpan(text: 'own', style: TextStyle(fontSize: 30, color: Colors.black)),
-                  ]
-                ),
+                    text: 'Create ',
+                    style: TextStyle(fontSize: 30, color: Colors.black),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: 'your ',
+                          style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black)),
+                      TextSpan(
+                          text: 'own',
+                          style: TextStyle(fontSize: 30, color: Colors.black)),
+                    ]),
               ),
               SizedBox(height: 157),
               SizedBox(
@@ -92,15 +100,28 @@ class HomeScreen extends StatelessWidget {
                     );
                   },
                   color: Colors.white,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(12))),
-                  child: Text("Make Bowl   ➜", style: TextStyle(color: Color(0xFFEB9A00), fontSize: 30, fontWeight: FontWeight.bold)),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(12))),
+                  child: Text("Make Bowl   ➜",
+                      style: TextStyle(
+                          color: Color(0xFFEB9A00),
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold)),
                 ),
               )
             ],
           ),
         ),
-
       ),
     );
+  }
+
+  Future<List<dynamic>> getAPIChoices(String type) async {
+    final response = await http.get("http://10.0.2.2:8000/ingredients.json");
+    if (response.statusCode == 200) {
+      Map<dynamic, dynamic> ingredients = jsonDecode(response.body);
+      return ingredients['ingredients'][type];
+    }
+    return [];
   }
 }

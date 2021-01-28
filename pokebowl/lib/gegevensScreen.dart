a4ece4625899a3a51dafb662b6945e01363398e4
Brian Loman <brianloman@hotmail.com>
2021-01-28 17:50:29 +0100

    Gegevens row fixed

--- pokebowl/lib/gegevensScreen.dart
+++ pokebowl/lib/gegevensScreen.dart
@@ -13,30 +13,55 @@ class GegevensScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      body: Padding(
+      body: Padding (
         padding: const EdgeInsets.fromLTRB(20.0, 50, 0, 0),
         child: Column(
           children: [
-            TextField(
-              decoration: InputDecoration(
-                hintText: "Your Name",
-                labelText: "Name",
-                hintStyle: TextStyle(fontSize: 12),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.orange)
+            Row(
+              children: [
+                Flexible(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: "Your Name",
+                        labelText: "Name",
+                        hintStyle: TextStyle(fontSize: 12),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.orange)
+                        ),
+                        labelStyle: TextStyle(
+                            fontSize: 15,
+                            color: Colors.orange
+                        ),
+                        border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.deepOrangeAccent) )
+                    ),
+                  ),
                 ),
-                labelStyle: TextStyle(
-                  fontSize: 15,
-                  color: Colors.orange
+                Flexible(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: "Your Name",
+                        labelText: "Name",
+                        hintStyle: TextStyle(fontSize: 12),
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.orange)
+                        ),
+                        labelStyle: TextStyle(
+                            fontSize: 15,
+                            color: Colors.orange
+                        ),
+                        border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(30))),
+                        focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.deepOrangeAccent) )
+                    ),
+                  ),
                 ),
-                border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(30)) ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.deepOrangeAccent) )
-              ) ,),
-          ],),
+              ]
+            ),
+          ],
+        ),
       ),
-        
-
     );
   }
 }

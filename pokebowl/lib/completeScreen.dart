27247ad882c6c276c31d12a0b6b282be4e7b2b45
Brian Loman <brianloman@hotmail.com>
2021-01-29 15:50:09 +0100

    Complete screen finished

--- pokebowl/lib/completeScreen.dart
+++ pokebowl/lib/completeScreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'main.dart';
 
 class Complete extends StatelessWidget {
   @override
@@ -15,7 +15,43 @@ class CompleteScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: Center(
-
+        child: Column(
+          children: [
+            SizedBox(height: 60),
+            Text("Thank you for the order",
+                style: TextStyle(
+                  shadows: [Shadow(color: Color(0xFFEB9A00), offset: Offset(0, -5))],
+                  color: Colors.transparent,
+                  fontSize: 25,
+                  decoration: TextDecoration.underline,
+                  decorationColor: Color(0xFFEB9A00),
+                  decorationThickness: 3,
+                )),
+            SizedBox(height: 40),
+            Image.asset('assets/images/Map.png'),
+            SizedBox(height: 20),
+            Text("Your delivery is on its way",
+            style: TextStyle(color: Color(0xFFEB9A00), fontSize: 21, fontWeight: FontWeight.bold)),
+            SizedBox(height: 20),
+            Text("Your delivery will arrive in about 15 minutes",
+            style: TextStyle(color: Colors.black, fontSize: 13, fontWeight: FontWeight.bold)),
+            SizedBox(height: 40),
+            SizedBox(
+              width: 212,
+              height: 50,
+              child: RaisedButton(onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomeScreen()),
+                );
+              },
+                color: Colors.white,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: Text("Back to home", style: TextStyle(color: Color(0xFFEB9A00), fontSize: 20, fontWeight: FontWeight.bold)),
+              ),
+            ),
+          ],
+        )
       ),
     );
   }
